5167903fe7b14216f0cd1fbc79a4fac971039da7
Leonardo <leonardo.araujornb@gmail.com>
2024-06-28 12:14:06 -0300

    fix: inserindo o botão de whatsapp

--- resources/views/site/layout/app.blade.php
+++ resources/views/site/layout/app.blade.php
@@ -36,6 +36,12 @@
         </section>
     </section>
 
+    <div id="whatsapp">
+        <a href="javascript:void(0)">
+            <i class="fa-brands fa-whatsapp"></i>
+        </a>
+    </div>
+
     <script type="text/javascript" src="{{ asset('assets/js/jquery.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/js/all.min.js"></script>
